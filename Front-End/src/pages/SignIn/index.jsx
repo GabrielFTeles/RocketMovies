c12fe89af0c0e5bbf562e836bbfda8588023c5ac
Gabriel Teles <101151753+GabrielFTeles@users.noreply.github.com>
2023-07-31 18:08:40 -0300

    ✨ feat: login email and password provided verification.

--- Front-End/src/pages/SignIn/index.jsx
+++ Front-End/src/pages/SignIn/index.jsx
@@ -24,6 +24,7 @@ export function SignIn() {
   const { signIn } = useAuth();
 
   function handleSignIn() {
+    if (!email || !password) return toast.error("Please provide email and password.");
     debounceSignIn();
   }
 
