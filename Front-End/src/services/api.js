6829bb5e805ac1204de73e2c96ef0270b781cffd
Gabriel Teles <101151753+GabrielFTeles@users.noreply.github.com>
2023-08-01 17:05:14 -0300

    Update api.js

--- Front-End/src/services/api.js
+++ Front-End/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "https://rocketmovies-api-jlhh.onrender.com"
+  baseURL: "https://api-rocketmovies-en3k.onrender.com"
 });
